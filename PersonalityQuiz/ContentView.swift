8680acbacbe3817330b4422d45bba45cc7ce74cd
Hamanilol <abdulrahmanmusa325@gmail.com>
2026-02-12 00:29:22 +0300

    Initial Commit

---
+++ PersonalityQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalityQuiz
+//
+//  Created by May Abdulla on 12/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalityQuiz/PersonalityQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalityQuizApp.swift
+//  PersonalityQuiz
+//
+//  Created by May Abdulla on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct PersonalityQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
